99b666b593cd0600ff40fdf4fb20476cd9bd0ba7
Vitor Augusto <vitoraugusto.szslv@gmail.com>
2026-03-04 20:56:02 +0000

    adicionada funcionalidade de imprimir retangulos do tamanho desejado pelo usuário

--- lab-02/ascii/src/main/java/ads/poo/Main.java
+++ lab-02/ascii/src/main/java/ads/poo/Main.java
@@ -17,19 +17,95 @@ public class Main {
                 }
                 System.out.println("");
             }
-
-            if (args[0].equalsIgnoreCase("triangulo") || args[0].equalsIgnoreCase("retângulo")){
-
-
-
-
-
-
-
         }
 
+        if (args[0].equalsIgnoreCase("retangulo") || args[0].equalsIgnoreCase("retângulo")){
+            
+            int largura = Integer.parseInt(args[1]);
+            int altura = Integer.parseInt(args[2]);
+            
+            // for para trabalhar de linha em linha
+            for (int i = 0; i < altura; i++) {
+
+                //se for a última ou primeira linha
+                if (i == 0 || i == altura-1){
+                    for (int j = 0; j < largura ; j++) {
+                        System.out.print(". ");
+                    }
+                    System.out.println(""); // pula linha
+                }
+
+                else { // se for as linhas do meio (vazadas)
+
+                    System.out.print(".");
+
+                    for (int j = 0; j < largura; j++) {
+                        System.out.print(" ");
+                    }
 
 
+                    if (largura>=5) {//ajusta o número de espaços de acordo com o número de colunas para fechar o desenho
+                        for (int j = 0; j < largura-3; j++) {
+                         System.out.print(" ");   
+                        }
+                    }
+
+                    System.out.println(".");
+                }
+            }
+        
         }
+
+        if (args[0].equalsIgnoreCase("losango")){
+
+            /*
+                *
+              * * *
+            * * * * *
+              * * *  
+                *
+                 
+                 
+                 
+                  *
+                * * *
+              * * * * *
+            * * * * * * *
+              * * * * *
+                * * *
+                  * 
+            */
+
+            int largura = Integer.parseInt(args[1]);
+            int espacos = largura-1;
+
+            for (int i = 0; i < largura; i++) {
+
+                // isso aqui fechou
+                for (int j = 0; j < espacos-Math.pow(2, i); j++) {
+                    System.out.print(" ");
+                }
+                
+
+                if (i==0){
+                    System.out.println("*");
+                }else if (i%2==0){//se é par
+                    for (int j = 0; j < i+1; j++) {
+                        System.out.print("* ");
+                    }
+                    System.out.println("");
+                } else {// se é ímpar
+                    for (int j = 0; j < i+2; j++) {
+                        System.out.print("* ");
+                    }    
+                    System.out.println("");
+                }
+
+            }
+            
+        }
+        
+
     }
+}
 
